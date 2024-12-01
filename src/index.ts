604e0952c0c6dd8590536917c8276a72ad0c8309
zoooeKao <kaoyw0619@gmail.com>
2024-12-01 23:33:20 +0800

    advise

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { serve } from '@hono/node-server';
-import { serveStatic } from '@hono/node-server/serve-static';
-import { Hono } from 'hono';
-import { logger } from 'hono/logger';
-import { cartRoutes } from './model/cart/routes';
-import { productRoutes } from './model/product/routes';
-import { todoRoutes } from './model/todo/route';
-import { userRoutes } from './model/user/routes';
-import { authRoutes } from './routes/auth';
+import {serve} from '@hono/node-server';
+import {serveStatic} from '@hono/node-server/serve-static';
+import {Hono} from 'hono';
+import {logger} from 'hono/logger';
+import {cartRoutes} from './model/cart/routes';
+import {productRoutes} from './model/product/routes';
+import {todoRoutes} from './model/todo/route';
+import {userRoutes} from './model/user/routes';
+import {authRoutes} from './routes/auth';
 
 const app = new Hono();
 
 app.use(logger());
-app.use('/*', serveStatic({ root: './public' }));
+app.use('/*', serveStatic({root: './public'}));
 
 app.route('/api/auth', authRoutes);
 app.route('/api/user', userRoutes);
@@ -19,7 +19,7 @@ app.route('/api/todo', todoRoutes);
 app.route('/api/cart', cartRoutes);
 app.route('/api/product', productRoutes);
 
-app.get('/', c => {
+app.get('/', (c) => {
   return c.html(`<div><h1>Create an index.html under the public folder</h1></div>`);
 });
 
@@ -28,7 +28,8 @@ app.onError((err, ctx) => {
   return ctx.text('Internal service error');
 });
 
-const port = 4000;
+// @ts-ignore
+const port = process.env.PORT || 4000;
 console.log(`Server is running on port ${port} at ${new Date()}`);
 
-serve({ fetch: app.fetch, port });
+serve({fetch: app.fetch, port: +port});
